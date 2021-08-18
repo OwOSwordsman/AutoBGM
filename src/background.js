cddc190c59adeef0ed9427a354354c533b42236e
OwO Swordsman <owoswordsman@gmail.com>
2021-08-17 20:06:41 -0400

    Implement playing and pausing audio

--- src/background.js
+++ src/background.js
@@ -1,6 +1,12 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     isAudible = changeInfo.audible
-    if (isAudible != null && tab.pinned) {
+    if (isAudible != null && !tab.pinned) {
         console.log("Audio: " + changeInfo.audible);
+        chrome.tabs.query({ pinned: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript({
+                files: ['controlAudio.js'],
+                target: { tabId: tabs[0].id }
+            });
+        });
     }
 });
---
+++ src/controlAudio.js
@@ -0,0 +1,5 @@
+document.dispatchEvent(new KeyboardEvent("keydown", {
+    key: " ",
+    keyCode: 32,        // example values.
+    code: '"Spacebar"',       // put everything you need in this object.
+}));
