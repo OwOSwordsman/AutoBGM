9cb30fbd2cad93187425612fad5b81c663ba01fe
OwO Swordsman <owoswordsman@gmail.com>
2021-08-18 11:21:38 -0400

    Implement checks for multiple audio sources
    Multiple tabs playing audio should no longer cause the music tab to
    resume playing audio when there are still other tabs playing audio.

--- src/background.js
+++ src/background.js
@@ -8,24 +8,29 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     // get first pinned tab
     chrome.tabs.query({ pinned: true }, function (tabs) {
         musicTab = tabs[0];
-        console.log(autoPaused); // LOG
 
         if (tab.id == musicTab.id && isReloaded) {   // reset autoPaused flag on pinned tab reload
             autoPaused = false;
-            
+
         // current tab is not target music tab and either music is playing or was auto paused previously
         } else if ((autoPaused || musicTab.audible) && (tab.id != musicTab.id && isAudible != null)) {
-            // set autoPaused flag
-            if (isAudible) {
-                autoPaused = true;
-            } else {
-                autoPaused = false;
-            }
-
-            // run js on first pinned tab
-            chrome.scripting.executeScript({
-                files: ['controlAudio.js'],
-                target: { tabId: musicTab.id }
+            // checks for when multiple tabs may be playing audio
+            chrome.tabs.query({ pinned: false, audible: true }, function (playingTabs) {
+                // set autoPaused flag
+                if (isAudible && !autoPaused) { // music has not been auto paused yet and another tab is playing audio
+                    autoPaused = true;
+                // music has been auto paused and all tabs are no longer playing audio
+                } else if (!isAudible && autoPaused && playingTabs.length == 0) {
+                    autoPaused = false;
+                } else {    // criteria not met, exit
+                    return;
+                }
+                
+                // run js on first pinned tab
+                chrome.scripting.executeScript({
+                    files: ['controlAudio.js'],
+                    target: { tabId: musicTab.id }
+                });
             });
         }
     });
