f46c0015196c8f6c9ceac494a9a09e61bb252c1c
OwO Swordsman <owoswordsman@gmail.com>
2021-08-18 11:48:45 -0400

    Use storage api for staring autoPause bool
    This way even when the service working is inactive. The bool value is
    still retrievable.

--- src/background.js
+++ src/background.js
@@ -1,6 +1,4 @@
 // listen for tab updates
-let autoPaused = false;
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     isAudible = changeInfo.audible;
     isReloaded = changeInfo.status;
@@ -10,27 +8,34 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         musicTab = tabs[0];
 
         if (tab.id == musicTab.id && isReloaded) {   // reset autoPaused flag on pinned tab reload
-            autoPaused = false;
+            chrome.storage.local.set({ "autoPaused": false });
 
-        // current tab is not target music tab and either music is playing or was auto paused previously
-        } else if ((autoPaused || musicTab.audible) && (tab.id != musicTab.id && isAudible != null)) {
-            // checks for when multiple tabs may be playing audio
-            chrome.tabs.query({ pinned: false, audible: true }, function (playingTabs) {
-                // set autoPaused flag
-                if (isAudible && !autoPaused) { // music has not been auto paused yet and another tab is playing audio
-                    autoPaused = true;
-                // music has been auto paused and all tabs are no longer playing audio
-                } else if (!isAudible && autoPaused && playingTabs.length == 0) {
-                    autoPaused = false;
-                } else {    // criteria not met, exit
-                    return;
+            // current tab is not target music tab and either music is playing or was auto paused previously
+        } else {
+            // retrieve autoPaused value from local storage
+            chrome.storage.local.get("autoPaused", function (key) {
+                autoPaused = key.autoPaused;
+                if ((autoPaused || musicTab.audible) && (tab.id != musicTab.id && isAudible != null)) {
+                    // checks for when multiple tabs may be playing audio
+                    chrome.tabs.query({ pinned: false, audible: true }, function (playingTabs) {
+                        // set autoPaused flag
+                        if (isAudible && !autoPaused) { // music has not been auto paused yet and another tab is playing audio
+                            autoPaused = true;
+                            // music has been auto paused and all tabs are no longer playing audio
+                        } else if (!isAudible && autoPaused && playingTabs.length == 0) {
+                            autoPaused = false;
+                        } else {    // criteria not met, exit
+                            return;
+                        }
+                        chrome.storage.local.set({ "autoPaused": autoPaused });
+
+                        // run js on first pinned tab
+                        chrome.scripting.executeScript({
+                            files: ['controlAudio.js'],
+                            target: { tabId: musicTab.id }
+                        });
+                    });
                 }
-                
-                // run js on first pinned tab
-                chrome.scripting.executeScript({
-                    files: ['controlAudio.js'],
-                    target: { tabId: musicTab.id }
-                });
             });
         }
     });
