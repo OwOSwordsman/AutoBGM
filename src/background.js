695ddfea59eb70701aedc543e564b820e8d859d8
OwO Swordsman <owoswordsman@gmail.com>
2021-08-18 09:06:40 -0400

    Add comments

--- src/background.js
+++ src/background.js
@@ -1,7 +1,10 @@
+// listen for tab updates
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log(changeInfo); // LOG
     isAudible = changeInfo.audible
-    if (isAudible != null && !tab.pinned) {
-        console.log("Audio: " + changeInfo.audible);
+
+    if (isAudible != null && !tab.pinned) { // audio for tab changed and is not pinned
+        // run js on first pinned tab
         chrome.tabs.query({ pinned: true, currentWindow: true }, function (tabs) {
             chrome.scripting.executeScript({
                 files: ['controlAudio.js'],
--- src/controlAudio.js
+++ src/controlAudio.js
@@ -1,5 +1,6 @@
+// simulate spacebar
 document.dispatchEvent(new KeyboardEvent("keydown", {
     key: " ",
-    keyCode: 32,        // example values.
-    code: '"Spacebar"',       // put everything you need in this object.
+    keyCode: 32,
+    code: '"Spacebar"'
 }));
